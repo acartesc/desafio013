7f4460e714211b6f756782502741d832c9839ffb
Andres <acartescc@gmail.com>
2017-08-28 20:08:37 -0300

    ejercicio 3 terminado

---
+++ ejercicio3.rb
@@ -0,0 +1,16 @@
+h = {x: 1, y:2}
+
+h[:z] = 3
+
+h[:x] = 5
+
+h.delete(:y)
+
+h.each { |key, value| puts "yeeah" if key == :z }
+
+#h.each_key {|k| puts "yeeah" if key == :z} (otra forma solo con key)
+
+b = {}
+h.each { |key, value| b[value] = key }
+
+puts b
