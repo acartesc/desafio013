b8869245191745a31590277b2d73594011df4a60
Andres <acartescc@gmail.com>
2017-08-28 20:24:53 -0300

    ejercicio 4 terminado

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,1 +1,13 @@
-ejercicio4.rb
+personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
+edades = [32, 28, 41, 19]
+
+personas_hash = Hash[personas.zip edades] #{"Carolina"=>32, "Alejandro"=>28, "Maria Jesús"=>41, "Valentín"=>19}
+
+puts '##################'
+
+def wierd_method(hash, key)
+  puts hash[key]
+end
+
+wierd_method(personas_hash, "Alejandro")
+
